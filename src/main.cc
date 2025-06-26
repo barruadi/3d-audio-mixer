ffd7f4746764d5764597f24196c608f733e4ad06
Barru Adi <barru.adi@gmail.com>
2025-06-26 22:33:34 +0700

    add: glad lib

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main() {
@@ -10,9 +11,20 @@ int main() {
 
     window = glfwCreateWindow(600, 480, "Window", NULL, NULL);
 
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window)) {
 
         glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
 
     }
 
