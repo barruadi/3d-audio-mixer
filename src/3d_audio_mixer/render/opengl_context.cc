d3a34cf0693a97897fc5e29800cbbe6bed2e8f2b
Barru Adi <barru.adi@gmail.com>
2025-08-30 21:44:08 +0700

    cleanup: indentation missalign

--- src/3d_audio_mixer/render/opengl_context.cc
+++ src/3d_audio_mixer/render/opengl_context.cc
@@ -37,7 +37,7 @@ namespace nrender
             return false;
         }
 
-                glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         #ifdef __APPLE__
