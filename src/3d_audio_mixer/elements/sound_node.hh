eb277d629efbcfc91c1b7e002e13886a05c2bf4d
Barru Adi <barru.adi@gmail.com>
2025-08-31 12:44:54 +0700

    feat: sound node calss

---
+++ src/3d_audio_mixer/elements/sound_node.hh
@@ -0,0 +1,31 @@
+namespace nelement
+{
+    class SoundNode
+    {
+        private:
+            std::string mFile;
+
+            // properties
+            glm::vec3 mPosition;
+            float mVolume;
+            float mPan;
+
+        public:
+            SoundNode():
+                mFile(""),
+                mPosition(0.0f, 0.0f, 0.0f),
+                mVolume(1.0f),
+                mPan(0.0f)
+            {
+
+            }
+
+            void update(const std::string& file, const glm::vec3& position, float volume, float pan)
+            {
+                mFile = file;
+                mPosition = position;
+                mVolume = volume;
+                mPan = pan;
+            }
+    };
+} // namespace nelement
